644fdd7681eafa32fdd429cde0e48badc4f6f346
Antoine FAUROUX <antoine.fauroux@unamur.be>
2019-07-11 00:56:06 +0200

    new struct based on id system and only one input c

--- neuron.go
+++ neuron.go
@@ -5,34 +5,32 @@ package neurons
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-var mainclock = time.NewTicker(500 * time.Millisecond)
+// constantes and globals
+const dtNeurones = 50 * time.Millisecond  // time between updates
+const dtSensors = 1000 * time.Millisecond // time between sensors firing
+const buffSize = 100                      // buffer size of the input chanel
+const tresh = 100                         // potential reaching this causes firing
+const damping = 1                         // potential lost in one DT
+const maxSig = 50                         // maximum signal strengh
+var nbNeurones = 0                        // total nb of neurons, used to generate unique ids
 
-// Axone neurons output chanel
-type Axone struct {
-	pre, pos *Neuron
-	C        chan int
-}
-
-// Synapse neurons imput chanels
-type Synapse struct {
-	pre, post *Neuron
-	C         chan int
-	weight    int
+// generateID generate IDs for new neurons
+func generateID() int {
+	nbNeurones++
+	return nbNeurones
 }
 
 // Neuron the default brick
 type Neuron struct {
-	id      int             // id (total number)
-	input   chan<- int      // every presynaptic Neuron sends his id
-	outputs []chan int      // input chanels of all Neurons listening to this one
-	weights map[int]int     // from the id we know the associated
-	parents map[int]*Neuron // Neuron and weight
-
-	clock          time.Ticker //internal clock for potential updates
+	id             int             // id (total number)
+	input          chan int        // every presynaptic Neuron sends his id
+	weights        map[int]int     // from the id we know the associated
+	parents        map[int]*Neuron // Neuron and weight
+	childs         map[int]*Neuron // input chanels of all Neurons listening to this one
+	clock          *time.Ticker    //internal clock for potential updates
 	thresh         int
 	damping        int
 	potential      int
@@ -41,67 +39,50 @@ type Neuron struct {
 
 // NewNeuron creates a neuron with default values
 func NewNeuron() *Neuron {
-	return &Neuron{}
+	return &Neuron{
+		id:        nbNeurones,
+		input:     make(chan int, buffSize),
+		weights:   make(map[int]int),
+		parents:   make(map[int]*Neuron),
+		childs:    make(map[int]*Neuron),
+		clock:     time.NewTicker(dtNeurones),
+		potential: 0,
+		verbose:   true,
+		alive:     true,
+	}
 }
 
 // Fire a neuron when its potential is above the threshold
 func (n *Neuron) Fire() {
-	for _, out := range n.outputs {
-		out <- n.id
+	for _, post := range n.childs {
+		post.input <- n.id
 	}
 }
 
 // Update a neuron potential whenever it receive a msg from a dendrite
 func (n *Neuron) Update() {
-	for t := range n.clock.C {
-		// lower the potential according to dunping and reads
-		// inputs from the input chanel buffer
-		fmt.Println(t)
+	for n.alive {
+		select {
+		case <-n.clock.C:
+			n.potential -= damping
+			if n.potential <= 0 {
+				n.potential = 0
+			}
+			if n.verbose {
+				fmt.Printf("Nr%v: %v", n.id, n.potential)
+			}
+		case id := <-n.input:
+			n.potential += n.weights[id]
+			if n.potential >= tresh {
+				n.Fire()
+			}
+		}
 	}
+	close(n.input) //closing input should be catched by parents
 }
 
 // Sensor are sensitive neurons that fires at regular rates
 type Sensor struct {
-	axone     *Axone
 	potential int
 	ticker    *time.Ticker
-}
-
-// NewAxone creates an axone with default values
-func NewAxone(pre, pos *Neuron) *Axone {
-	C := make(chan int)
-	return &Axone{
-		pre: pre,
-		pos: pos,
-		C:   C,
-	}
-
-}
-
-// NewSensor creates a sensor with default values
-func NewSensor() *Sensor {
-	var dt = 500 * time.Millisecond
-	var ticker = time.NewTicker(dt)
-	return &Sensor{
-		axone:     NewAxone(nil, nil),
-		potential: 10,
-		ticker:    ticker,
-	}
-}
-
-func combine(inputs []<-chan int, output chan<- int) {
-	var group sync.WaitGroup
-	for i := range inputs {
-		group.Add(1)
-		go func(input <-chan int) {
-			for val := range input {
-				output <- val
-			}
-			group.Done()
-		}(inputs[i])
-	}
-	go func() {
-		group.Wait()
-		close(output)
-	}()
 }
